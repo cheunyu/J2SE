44b510d2089904d653c31a39fbf9c4d69139c49c
cheunyu <cheunyufoo@gmail.com>
2018-04-21 17:21:58 +0800

    io.file

---
+++ src/io/Test.java
@@ -0,0 +1,35 @@
+package io;
+
+import java.io.File;
+
+/**
+ * @author:cheunyu
+ * @date:2018/4/21 17:21
+ */
+public class Test {
+    public static void main(String[] args) {
+        String path = "d:/photo";
+        File file = new File(path);
+        printName(file);
+        File[] fileArrays = File.listRoots();
+        for (File f:fileArrays) {
+            printName(f);
+        }
+    }
+
+    public static void printName(File file) {
+        if (file == null || !file.exists()) {
+            return;
+        }
+        System.out.println(file.getAbsolutePath());
+        if (file.isDirectory()) {
+            File[] fileArray = file.listFiles();
+            if (fileArray != null) {
+                for (File f:fileArray) {
+                    printName(f);
+                }
+            }
+
+        }
+    }
+}
