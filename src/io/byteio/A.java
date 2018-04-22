4adabe9285882ea74c61ca530571f63deb6a0530
cheunyu <cheunyufoo@gmail.com>
2018-04-22 15:25:31 +0800

    io.file
    FileReader,FileWriter,File&FloderCopy

---
+++ src/io/byteio/A.java
@@ -0,0 +1,56 @@
+package io.byteio;
+
+import java.io.*;
+
+/**
+ * @author:cheunyu
+ * @date:2018/4/22 15:24
+ */
+public class A {
+    public static void main(String[] args) {
+        String path = "C:/Users/foo/Desktop/新建文件夹/test1.txt";
+        String path2 = "C:/Users/foo/Desktop/新建文件夹/test1.txt";
+        copy(path, path2);
+    }
+
+    public static void copy(String path, String path2) {
+        File file = new File(path);
+        File file2 = new File(path2);
+        copy(file, file2);
+    }
+
+    public static void copy(File file, File file2) {
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        try {
+            inputStream = new FileInputStream(file);
+            outputStream = new FileOutputStream(file2,true);
+            byte[] bytes = new byte[1024];
+            int len = 0;
+            while (-1!=(len = inputStream.read(bytes))) {
+                outputStream.write(bytes, 0 ,len);  //防止最后一次byte数组内容尾部有空值，即输入流读出多少，输出流写入多少
+            }
+            outputStream.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 先打开的后关闭原则。先关闭输出流吗，再关闭输入流
+            if (outputStream != null ) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (inputStream != null ) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
