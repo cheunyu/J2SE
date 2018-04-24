3d3ea9d704f12491765a7aaa91a6b4fc1a7847b8
cheunyu <cheunyufoo@gmail.com>
2018-04-24 20:38:13 +0800

    socket InetAddress InetSocketAddress

---
+++ src/socket/InetAddressExample.java
@@ -0,0 +1,18 @@
+package socket;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author:cheunyu
+ * @date:2018/4/24 19:58
+ */
+public class InetAddressExample {
+
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress inetAddress = InetAddress.getByName("www.douban.com");
+        System.out.println(inetAddress.getHostName());
+        System.out.println(inetAddress.getHostAddress());
+
+    }
+}
---
+++ src/socket/InetSocketAddressExample.java
@@ -0,0 +1,21 @@
+package socket;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author:cheunyu
+ * @date:2018/4/24 20:02
+ */
+public class InetSocketAddressExample {
+
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress inetAddress = InetAddress.getByName("www.douban.com");
+        InetSocketAddress inetSocketAddress = new InetSocketAddress(inetAddress, 100);
+        System.out.println(inetSocketAddress.getHostName());
+        System.out.println(inetSocketAddress.getHostString());
+        System.out.println(inetSocketAddress.getPort());
+        System.out.println(inetSocketAddress.getAddress());
+    }
+}
